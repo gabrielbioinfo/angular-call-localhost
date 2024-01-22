fddadeae6221ec5751a807ca7be81c6ab77c87ff
Gabriel Espindola <gabrielbioinfo@gmail.com>
2024-01-22 17:16:39 -0300

    initializing the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `<h1>{{ title }}</h1>
+    <hr />
+    {{ response() }}`,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-call-localhost';
+  response = signal<string>('iniciando sem nada ainda...');
+
+  ngOnInit() {
+    fetch('http://localhost:3000/')
+      .then((response) => response.text())
+      .then((data) => this.response.set(data))
+      .catch((err) => this.response.set(err));
+  }
 }
