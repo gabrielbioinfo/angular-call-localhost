a07a73b3752f430abf1f37bbab16fd8e18740e7e
Gabriel Espindola <gabrielbioinfo@gmail.com>
2024-01-23 07:32:54 -0300

    feat: adds socket io

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { io } from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +8,21 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   template: `<h1>{{ title }}</h1>
     <hr />
-    {{ response() }}`,
-  styleUrl: './app.component.css',
+    <h3>Fetch:</h3>
+    <div style="border: 1px solid;padding: 3px;width:100%">
+      {{ response() }}
+    </div>
+    <hr />
+    <h3>Web Socket:</h3>
+    <div style="border: 1px solid;padding: 3px;width:100%">
+      {{ wsResponse() }}
+    </div>`,
+  styles: [],
 })
 export class AppComponent {
   title = 'angular-call-localhost';
   response = signal<string>('iniciando sem nada ainda...');
+  wsResponse = signal<string>('');
 
   ngOnInit() {
     fetch('http://localhost:3000/')
@@ -23,5 +33,15 @@ export class AppComponent {
           'Servidor offline! Por favor ligue seu servidor em http://localhost:3000.'
         )
       );
+    const socket = io('http://localhost:3000');
+    socket.on('connect', () => {
+      this.response.set('Conectado ao servidor!');
+    });
+    socket.on('message', (data) => {
+      this.response.set(data);
+    });
+    socket.on('disconnect', () => {
+      this.response.set('Desconectado do servidor!');
+    });
   }
 }
