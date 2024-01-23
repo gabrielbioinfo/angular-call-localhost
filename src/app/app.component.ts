4f0b8fbb42356b96148bd926a589c192e296713c
Gabriel Espindola <gabrielbioinfo@gmail.com>
2024-01-23 09:02:21 -0300

    feat: send message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { io } from 'socket.io-client';
+import { DefaultEventsMap } from '@socket.io/component-emitter';
+import { Socket, io } from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
@@ -33,15 +34,31 @@ import { io } from 'socket.io-client';
     <hr />
     <div style="border: 1px solid;padding: 3px;width:100%;min-height: 30px">
       {{ wsResponse() }}
-    </div>`,
+    </div>
+    <hr />
+    <h3>Web Socket Message:</h3>
+    <input
+      type="url"
+      class="form-control"
+      [(ngModel)]="wsMessage"
+      placeholder="http://localhost:3000"
+    />
+    <button (click)="sendMessage()">WebSocket: Enviar Mensagem</button>
+    <hr />
+    <div style="border: 1px solid;padding: 3px;width:100%;min-height: 30px">
+      {{ wsMessageResponse() }}
+    </div> `,
   styles: [],
 })
 export class AppComponent {
   title = 'angular-call-localhost';
   response = signal<string>('iniciando sem nada ainda...');
   wsResponse = signal<string>('');
+  wsMessageResponse = signal<string>('');
   apiLocation = '';
   webSocketLocation = '';
+  wsMessage = '';
+  socket: Socket<DefaultEventsMap, DefaultEventsMap> | undefined = undefined;
 
   getDataFromApi() {
     //'http://localhost:3000/'
@@ -67,15 +84,24 @@ export class AppComponent {
 
   initializeWebSocket() {
     //'http://localhost:3000/'
-    const socket = io(this.webSocketLocation);
-    socket.on('connect', () => {
+    this.socket = io(this.webSocketLocation);
+    this.socket.on('connect', () => {
       this.response.set('Conectado ao servidor!');
     });
-    socket.on('message', (data) => {
+    this.socket.on('message', (data) => {
       this.response.set(data);
     });
-    socket.on('disconnect', () => {
+    this.socket.on('disconnect', () => {
       this.response.set('Desconectado do servidor!');
     });
+  }
+
+  sendMessage() {
+    if (this.socket) {
+      this.socket.emit(
+        'message',
+        this.wsMessage.length > 0 ? this.wsMessage : 'blank message'
+      );
+    }
   }
 }
